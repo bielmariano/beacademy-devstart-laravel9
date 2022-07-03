0b0e3d78fc4158daff8b290f9ad8642a863a8d32
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-07-03 11:39:23 -0300

    AULA 06 | BLADE

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Usuários</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <h1>Listagem de Usuários</h1>
-        <table class="table">
+@extends('template.users')
+@section('title', 'Listagem de Usuários')
+@section('body')
+<h1>Listagem de Usuários</h1>
+<table class="table">
   <thead>
     <tr>
       <th scope="col">ID#</th>
@@ -34,6 +26,4 @@
 
   </tbody>
 </table>
-    </div>
-</body>
-</html>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuário {{$user->name}}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-</head>
-<body>
-    <div class="container">
+@extends('template.users')
+@section('title', 'Visualizar Usuário')
+@section('body')
         <h1>Listagem de Usuários</h1>
         <table class="table">
   <thead class="text-center">
@@ -33,6 +24,4 @@
 
   </tbody>
 </table>
-    </div>
-</body>
-</html>
+@endsection
