375605bad8617b48398d7b41388c3b42091a3660
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-07-11 19:20:47 -0300

    aula 02 - Imagem na lista de usuarios e paginacao

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(5);
 
         return view('users.index', compact('users'));
     }
@@ -49,9 +49,11 @@ class UserController extends Controller
         $data = $request->all();
         $data['password'] = bcrypt($request->password);
 
-        $file = $request['image'];
-        $path = $file->store('profile','public');
-        $data['image'] = $path;
+        if ($request->image) {
+            $file = $request['image'];
+            $path = $file->store('profile', 'public');
+            $data['image'] = $path;
+        }
 
         $this->model->create($data);
 
@@ -81,7 +83,7 @@ class UserController extends Controller
 
     public function destroy($id)
     {
-        if(!$user = $this->model->find($id))
+        if (!$user = $this->model->find($id))
             return redirect()->route('users.index');
 
         $user->delete();
@@ -89,4 +91,3 @@ class UserController extends Controller
         return redirect()->route('users.index');
     }
 }
-
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,11 +2,12 @@
 @section('title', 'Listagem de Usuários')
 @section('body')
 <h1>Listagem de Usuários</h1>
-<a href="{{route('users.create')}}" class="btn btn-success">Novo Usuário</a>
+<a href="{{route('users.create')}}" class="btn btn-outline-dark">Novo Usuário</a>
 
 <table class="table">
   <thead>
     <tr>
+      <th scope="col">Foto</th>
       <th scope="col">ID#</th>
       <th scope="col">Nome</th>
       <th scope="col">Email</th>
@@ -17,15 +18,23 @@
   <tbody>
     @foreach($users as $user)
     <tr>
+      @if($user->image)
+      <th> <img src="{{ asset('storage/'.$user->image)}}" width="50px" height="50px" class="rounded-circle"></th>
+      @else
+      <th> <img src="{{ asset('storage/profile/avatar.png')}}" width="50px" height="50px" class="rounded-circle"></th>
+      @endif
       <th scope="row">{{ $user->id}}</th>
       <td>{{ $user->name}}</td>
       <td>{{$user->email}}</td>
       <td>{{date('d/m/Y - H:i', strtotime($user->created_at))}}</td>
-      <td> <a href="{{route('users.show', $user->id)}} " class="btn btn-info text-white">VISUALIZAR</a> </td>
+      <td> <a href="{{route('users.show', $user->id)}} " class="btn btn-primary text-white">VISUALIZAR</a> </td>
 
     </tr>
     @endforeach
 
   </tbody>
 </table>
+<div class="justify-content-center pagination">
+  {{$users->links('pagination::bootstrap-4')}}
+</div>
 @endsection
