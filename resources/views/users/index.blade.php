362c52a3deff9957ba361905aa6026422c2341f6
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-07-18 10:12:23 -0300

    Aula 07 - Auth Parte I

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,12 @@
 @extends('template.users')
 @section('title', 'Listagem de Usuários')
-@section('body')
-<h1>Listagem de Usuários</h1>
+@section ('body')
+<h1> Listagem de Usuários</h1>
 
 <div class="container">
   <div class="row">
     <div class="col-sm mt-2 mb-5">
-      <a href="{{ route('users.create') }}" class="btn btn-outline-dark">Novo Usuário</a>
+      <a href="{{ route('users.create') }}" class="btn btn-outline-dark"> Novo Usuário</a>
     </div>
     <div class="col-sm mt-2 mb-5">
       <form action="{{ route('users.index') }}" method="GET">
@@ -16,43 +16,47 @@
         </div>
       </form>
     </div>
-
   </div>
 </div>
 
 <table class="table">
-  <thead>
+  <thead class=" text-center">
     <tr>
       <th scope="col">Foto</th>
-      <th scope="col">ID#</th>
+      <th scope="col">Id</th>
       <th scope="col">Nome</th>
       <th scope="col">Email</th>
       <th scope="col">Postagens</th>
-      <th scope="col">Data de Cadastro</th>
+      <th scope="col">Data Cadastro</th>
       <th scope="col">Ações</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody class=" text-center">
     @foreach($users as $user)
     <tr>
       @if($user->image)
-      <th> <img src="{{ asset('storage/'.$user->image)}}" width="50px" height="50px" class="rounded-circle"></th>
+      <th><img src=" {{ asset('storage/'.$user->image) }}" width="50px" height="50px" class="rounded-circle" /></th>
       @else
-      <th> <img src="{{ asset('storage/profile/avatar.png')}}" width="50px" height="50px" class="rounded-circle"></th>
+      <th><img src=" {{ asset('storage/profile/avatar.png') }}" width="50px" height="50px" class="rounded-circle" /></th>
       @endif
       <th scope="row">{{ $user->id}}</th>
-      <td>{{ $user->name}}</td>
-      <td>{{$user->email}}</td>
-      <td> <a href="{{route('posts.show', $user->id)}} " class="btn btn-outline-dark">Postagens: {{ $user->posts->count() }}</a> </td>
-      <td>{{date('d/m/Y - H:i', strtotime($user->created_at))}}</td>
-      <td> <a href="{{route('users.show', $user->id)}} " class="btn btn-primary text-white">VISUALIZAR</a> </td>
-
+      <td>{{ $user->name }}</td>
+      <td>{{ $user->email }}</td>
+      <td>
+        <a href="{{ route('posts.show', $user->id) }}" class="btn btn-outline-dark">Postagens - {{ $user->posts->count() }}</a>
+      </td>
+      <td>{{ date ('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
+      <td>
+        <a href="{{ route('users.show', $user->id) }}" class="btn btn-primary text-white">Visualizar</a>
+      </td>
+      </td>
     </tr>
     @endforeach
 
   </tbody>
 </table>
 <div class="justify-content-center pagination">
-  {{$users->links('pagination::bootstrap-4')}}
+  {{ $users->links('pagination::bootstrap-4') }}
+</div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,31 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{
-    UserController,
-    PostController,
-    ViaCepController
-};
-
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/users/{id}/posts', [PostController::class, 'show'])->name('posts.show');
-
-Route::delete('/users/{id}',[UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/user',[UserController::class, 'store'])->name('users.store');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 
-//VIA CEP WEB SERVICE
-Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
-Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
-Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
+require __DIR__ . '/auth.php';
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update')->middleware('auth');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit')->middleware('auth');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('auth');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create')->middleware('auth');
+    Route::post('/user', [UserController::class, 'store'])->name('users.store')->middleware('auth');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/users/{id}/posts', [PostController::class, 'show'])->name('posts.show');
+
+
+    //VIA CEP WEB SERVICE
+    Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+    Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+    Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
+});
